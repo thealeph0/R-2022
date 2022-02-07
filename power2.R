63f6a455beae5daf2dbf4772a50d6cad99f181cf
thealeph0 <84290656+thealeph0@users.noreply.github.com>
2022-02-07 13:48:58 -0800

    Update power2.R

--- power2.R
+++ power2.R
@@ -3,6 +3,9 @@ library(data.table)
 n <- readline(prompt="How many have been worked?: ")
 n1 <- as.numeric(n)
 
+if (n1<0)
+  print("Invalid number of days worked")
+
 n2 <- c(1:n1)
 
 v <- 2^(n2)*(0.01)
