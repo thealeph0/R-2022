879f67fdb6ae6fdc0cdb81924aff0fb43e5c1d5e
thealeph0 <84290656+thealeph0@users.noreply.github.com>
2022-02-07 19:34:06 -0800

    Update series.R

--- series.R
+++ series.R
@@ -26,3 +26,55 @@ repeat
 }  
 
 cat("The sum from 1 to", n1, "is: ",sum)
+
+
+ver2 using while and repeat
+
+sum <- 0
+i <- 1
+
+n <- readline(prompt="Enter a positive integer value: ")
+n1 <- as.numeric(n)
+
+while (n1 <=0)
+  {
+  cat("Please enter a POSITIVE integer")
+  }
+
+repeat
+{
+  
+  sum = sum + i
+  i = i+1
+  
+  if (i == n1+1)
+  {
+    break
+  }
+}  
+
+cat("The sum from 1 to", n1, "is: ",sum)
+
+
+ver 3 using while and while
+  
+  sum <- 0
+i <- 1
+
+n <- readline(prompt="Enter a positive integer value: ")
+n1 <- as.numeric(n)
+
+while (n1 <=0)
+  {
+  cat("Please enter a POSITIVE integer")
+  n <- readline(prompt="Enter a positive integer value: ")
+  n1 <- as.numeric(n)
+  }
+
+while (i <= n1)
+{
+  sum = sum + i
+  i = i+1
+}  
+
+cat("The sum from 1 to", n1, "is: ",sum)
